4c07317319c41b2aa6fa838d1b98d8421eadbed6
Eva Joubert <evajoubert@live.fr>
2023-06-21 17:51:37 +0200

    Like Controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  def create
+    # Récupérer l'utilisateur actuel
+    current_user = User.find(params[:current_user_id])
+
+    # Récupérer l'utilisateur aimé
+    liked_user = User.find(params[:liked_user_id])
+
+    # Créer une relation "like" entre les utilisateurs
+    current_user.likes.create(liked_user: liked_user)
+
+    # Vérifier si l'utilisateur aimé a également aimé l'utilisateur actuel
+    if liked_user.likes.exists?(liked_user: current_user)
+      # Si c'est le cas, créer une correspondance (match) entre les utilisateurs
+      Match.create(user1: current_user, user2: liked_user)
+    end
+  end
 end
