ab6d7547c81fa04cbfd4fbfeafb70cf8f8e14bdb
Fany Servier <fany.servier@gmail.com>
2023-06-24 11:35:56 +0200

    Search

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,28 @@
+class ProfilesController < ApplicationController
+  def show
+    @profile = User.find(params[:id])
+    @user_games = @profile.user_games
+  end
+
+  def edit
+    @profile = User.find(params[:id])
+  end
+
+  def update
+    puts params.inspect
+    @profile = User.find(params[:id])
+    if @profile.update(profile_params)
+      # Successful
+      redirect_to profile_path(@profile), notice: "User information updated successfully."
+    else
+      # Failed
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:username, :birthdate, :gender, :email, :password)
+  end
+end
--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,15 +1,15 @@
 class UserGamesController < ApplicationController
   def new
-    @user = User.find(params[:user_id])
-    @user_game = @user.user_games.build
+    @profile = User.find(params[:profile_id])
+    @user_game = @profile.user_games.build
     @games = Game.all
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @user_game = @user.user_games.build(user_game_params)
+    @profile = User.find(params[:profile_id])
+    @user_game = @profile.user_games.build(user_game_params)
     if @user_game.save
-      redirect_to user_path(@user), notice: "User game added successfully."
+      redirect_to profile_path(@profile), notice: "User game added successfully."
     else
       @games = Game.all
       render :new
@@ -23,22 +23,35 @@ class UserGamesController < ApplicationController
   def update
     @user_game = UserGame.find(params[:id])
     if @user_game.update(user_game_params)
-      redirect_to user_path(@user_game.user), notice: "User game information updated successfully."
+      redirect_to profile_path(@user_game.user), notice: "User game information updated successfully."
     else
       render :edit
     end
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    @user_game = @user.user_games.find(params[:id])
+    @profile = User.find(params[:profile_id])
+    @user_game = @profile.user_games.find(params[:id])
     @user_game.destroy
-    redirect_to user_path(@user)
+    redirect_to profile_path(@profile)
+  end
+
+  def search
+    # Récupérer l'id du profile/current_user
+    @profile = current_user
+    # Récupérer les jeux associés au profil
+    @user_games = @profile.user_games
+    # Effectuer la recherche en utilisant les paramètres de user_game
+    @search_results = @user_games.where(search_params)
   end
 
   private
 
   def user_game_params
     params.require(:user_game).permit(:level, :mood, :game_id)
+  end
+
+  def search_params
+    params.permit(:game_id, :level, :mood)
   end
 end
