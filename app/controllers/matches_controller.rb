07e853fb9e48315521dc4ca1f75a6745741545e8
Fany Servier <fany.servier@gmail.com>
2023-07-08 09:54:53 +0200

    match view front

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,5 +2,7 @@ class MatchesController < ApplicationController
   def show
     @user = User.find(params[:profile_id])
     @match = Match.find(params[:id])
+    @current_user = current_user
+    @liked_user = @match.like.liker
   end
 end
