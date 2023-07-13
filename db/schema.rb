434929260ca2bc6a367f42cd7dbc184eeea9f058
Reda Boukakiou <reda_boukakiou@hotmail.com>
2023-07-13 20:03:03 +0200

    flash messages devise

--- db/schema.rb
+++ db/schema.rb
@@ -122,6 +122,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_112203) do
     t.date "birthdate"
     t.string "gender"
     t.integer "age"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
