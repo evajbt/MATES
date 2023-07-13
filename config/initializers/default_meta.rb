2377b3bbafbc53a8c6aa5377f2103a6b9ec05233
Reda Boukakiou <reda_boukakiou@hotmail.com>
2023-07-13 17:46:59 +0200

    metadata

--- config/initializers/default_meta.rb
+++ config/initializers/default_meta.rb
@@ -1,4 +1,7 @@
 # config/initializers/default_meta.rb
 
 # Initialize default meta tags.
-DEFAULT_META = YAML.load_file(Rails.root.join("config/meta.yml"))
+DEFAULT_META = YAML.safe_load(
+  File.read(Rails.root.join("config/meta.yml")),
+  [Date]
+)
